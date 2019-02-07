92c84fb3d63c57d2661895db7b54e1c42ec21ae6
piyushnanwani <35925360+piyushnanwani@users.noreply.github.com>
2019-02-07 16:52:52 +0530

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,24 @@ import sys
 
 import pygame
 
+from ship import Ship
+from settings import Settings
+
 def run_game():
 	#initialise game and create a screen object.
 	pygame.init()
-	screen = pygame.display.set_mode((1200,700))
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
+	# screen = pygame.display.set_mode((1200,700))
+	
 	pygame.display.set_caption("Alien Invasion")
+
+	# Make a ship.
+	ship = Ship(screen)
+
+	# Set the background color.
+	bg_color = (230,230,230)
 
 	#Start the main loop of the game.
 	while True:
@@ -15,6 +28,11 @@ def run_game():
 		for event in pygame.event.get():
 			if event.type== pygame.QUIT:
 				sys.exit()
+		# Redraw the screen during wach pass through the loop.
+		screen.fill(ai_settings.bg_color)
+		ship.blitme()
 
 		# Make the recently drawn screen visible.
+		pygame.display.flip()
+
 run_game()
